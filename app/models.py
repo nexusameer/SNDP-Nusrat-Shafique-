1d88c0e2804f527f16c544d8bea24b0bc06db949
nexusameer <ameerk10fw@gmail.com>
2024-08-19 16:51:59 -0700

    model setup

--- app/models.py
+++ app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    facebook_profile = models.URLField(blank=True, null=True)
+    insta_profile = models.URLField(blank=True, null=True)
+    linkedin_profile = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    service_image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    types = models.ManyToManyField(Type)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from .models import *
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+
+        context['persona'] = Persona.objects.all()
+        context['category'] = Category.objects.all()
+        context['Service'] = Service.objects.all()
+        
+        return context
