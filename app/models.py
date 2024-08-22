d176b9033f6618320b395d1fa44886d57443b364
nexusameer <ameerk10fw@gmail.com>
2024-08-22 16:50:25 -0700

    email setup

--- app/models.py
+++ app/models.py
@@ -63,3 +63,12 @@ class Testimonal(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,15 +1,16 @@
-from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import *
+from django.core.mail import send_mail
+from .forms import *
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs: Any):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        context['form'] = ContactForm()
         context['persona'] = Persona.objects.all()
         context['category'] = Category.objects.all()
         context['services'] = Service.objects.all()
@@ -18,12 +19,41 @@ class HomeView(TemplateView):
         
         return context
 
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            
+            # Send email
+            send_mail(
+                subject,
+                f"Name: {name}\nEmail: {email}\nMessage: {message}",
+                email,
+                ['ameerh10fw@gmail.com'],
+                fail_silently=False,
+            )
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'index.html', {'form': form, 'persona': Persona.objects.all(), 'category': Category.objects.all(), 'services': Service.objects.all(), 'products':Product.objects.all(), 'testominals': Testimonal.objects.all()})
+
+
 class AboutView(TemplateView):
     template_name = 'about.html'
 
 
 class ProductView(TemplateView):
     template_name = 'products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['products'] = Product.objects.all()
+
+        return context
 
 
 class TeamView(TemplateView):
